63e57950399ebbce7ac28b26dd5d35bf8ef70a4b
Dawid Stepniewski <dawidstepniewski999@gmail.com>
2022-03-08 16:35:58 +0100

    FIX MASTER Repo

---
+++ QuickReading.Models/Profile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace QuickReading.Models
+{
+    public class Profile
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string ImageProfileUrl { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Surname { get; set; }
+
+        public string City { get; set; }
+
+        public string DescriptionProfile { get; set; }
+
+    }
+}
